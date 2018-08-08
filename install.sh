985ef0c160e556efe4836f3cdd4b7e2f5192d8a0
David Lam <dav_lam@ymail.com>
2018-08-07 22:15:39 -0700

    fixed install.sh; added iterm2 profile install

--- install.sh
+++ install.sh
@@ -12,11 +12,17 @@ curl -sSL https://get.rvm.io | bash -s stable
 # install oh-my-zsh (zsh easy install)
 echo "installing zsh shell\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm -rf ~/.zshrc && cp .zshrc ~/.zshrc
+rm -rf ~/.zshrc && cp .zshrc ~/
+mkdir -p ~/.oh-my-zsh/custom/themes
 cp steeef-lambda ~/.oh-my-zsh/custom/themes/steeef-lambda.zsh-theme
 source ~/.zshrc
 
 # setup vim
 echo "setting up vim\n"
-rm -rf ~/.vimrc && cp .vimrc ~/.vimrc
+rm -rf ~/.vimrc && cp .vimrc ~/
+
+# setup iterm2 profile (dynamic)
+echo "setting up iterm2 profile\n"
+mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
+cp iterm-profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 
