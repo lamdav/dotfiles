973c900853578465c56d5a337636e72eab68fa61
David Lam <dav_lam@ymail.com>
2019-05-18 23:11:36 -0700

    use symlink; add firewatch automation

--- install.sh
+++ install.sh
@@ -12,22 +12,25 @@ curl -sSL https://get.rvm.io | bash -s stable
 # install oh-my-zsh (zsh easy install)
 echo "installing zsh shell\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm -rf ~/.zshrc && cp .zshrc ~/
+rm -rf ~/.zshrc && ln -sf .zshrc ~/.zshrc
 mkdir -p ~/.oh-my-zsh/custom/themes
-cp steeef-lambda ~/.oh-my-zsh/custom/themes/steeef-lambda.zsh-theme
-rm -rf ~/.purepower && cp .purepower ~/
+ln -sf steeef-lambda.zsh-theme ~/.oh-my-zsh/custom/themes/steeef-lambda.zsh-theme
+rm -rf ~/.purepower && ln -sf .purepower ~/.purepower
 source ~/.zshrc
 updateplugins
+echo "run when completed: exec zsh\n"
 
 # setup vim
 echo "setting up vim\n"
-rm -rf ~/.vimrc && cp .vimrc ~/
+rm -rf ~/.vimrc && ln -sf .vimrc ~/.vimrc
 
 # setup git configurations
 echo "setting up gitconfig\n"
-rm -rf ~/.gitconfig && cp .gitconfig ~/
+rm -rf ~/.gitconfig && ln sf .gitconfig ~/.gitconfig
 
 # setup iterm2 profile (dynamic)
 echo "setting up iterm2 profile\n"
 mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
 cp iterm-profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/
+# prompts user to add color scheme
+open firewatch.itermcolors
