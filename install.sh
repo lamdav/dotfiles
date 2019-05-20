f664037efbbed52b100bdfc63f1430cf6c6b7fe0
David Lam <dav_lam@ymail.com>
2019-05-20 00:47:20 -0700

    fix install.sh linking

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export dir="$(pwd)"
+
 # install brew
 echo "installing homebrew\n"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -12,25 +14,25 @@ curl -sSL https://get.rvm.io | bash -s stable
 # install oh-my-zsh (zsh easy install)
 echo "installing zsh shell\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm -rf ~/.zshrc && ln -sf .zshrc ~/.zshrc
+rm -rf ~/.zshrc && ln -sf $dir/.zshrc ~/.zshrc
 mkdir -p ~/.oh-my-zsh/custom/themes
 ln -sf steeef-lambda.zsh-theme ~/.oh-my-zsh/custom/themes/steeef-lambda.zsh-theme
-rm -rf ~/.purepower && ln -sf .purepower ~/.purepower
+rm -rf ~/.purepower && ln -sf $dir/.purepower ~/.purepower
 source ~/.zshrc
 updateplugins
 echo "run when completed: exec zsh\n"
 
 # setup vim
 echo "setting up vim\n"
-rm -rf ~/.vimrc && ln -sf .vimrc ~/.vimrc
+rm -rf ~/.vimrc && ln -sf $dir/.vimrc ~/.vimrc
 
 # setup git configurations
 echo "setting up gitconfig\n"
-rm -rf ~/.gitconfig && ln -sf .gitconfig ~/.gitconfig
+rm -rf ~/.gitconfig && ln -sf $dir/.gitconfig ~/.gitconfig
 
 # setup iterm2 profile (dynamic)
 echo "setting up iterm2 profile\n"
 mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles
-ln -sf iterm-profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/iterm-profiles.json
+ln -sf $dir/iterm-profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/iterm-profiles.json
 # prompts user to add color scheme
 open firewatch.itermcolors
